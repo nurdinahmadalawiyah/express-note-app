f83e6fd94e3d29c3139f2a18e90a5c3ac780acb9
Nurdin Alawiyah <nurdinalawiyah@Nurdins-MacBook-Air.local>
2024-05-02 14:22:30 +0700

    change db default in mongoDB

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await // Connect to MongoDB
-            mongoose.connect(process.env.MONGO_URI, {
+        // Connect to MongoDB
+        await mongoose.connect(process.env.MONGO_URI, {
                 useNewUrlParser: true,
-                useUnifiedTopology: true
+                useUnifiedTopology: true,
+                dbName: 'notesapp'
             })
         console.log("MongoDB Connected")
     } catch (err) {
